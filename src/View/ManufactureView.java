a8781d040317266a23283ba1df85148f7d8b760f
ceo <ceo8099@g.rit.edu>
2019-03-24 17:50:09 -0400

    Created basic functionality of the welcome view

---
+++ src/View/ManufactureView.java
@@ -0,0 +1,11 @@
+package View;
+
+/**
+ * Created by ceo on 3/24/2019.
+ */
+public class ManufactureView implements View {
+    @Override
+    public void run() {
+
+    }
+}
--- src/View/WelcomeView.java
+++ src/View/WelcomeView.java
@@ -10,9 +10,30 @@ public class WelcomeView implements View {
     @Override
     public void run() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to: \n");
-        System.out.println("AUTO MANAGER 2K19 \n");
-        System.out.println("Please enter your role: \n");
+        System.out.println("Welcome to: ");
+        System.out.println("AUTO MANAGER 2K19 ");
+        System.out.println("Please enter your role: ");
+        System.out.println("[M]anufacture Employee");
+        System.out.println("[D]ealer");
+        System.out.println("[C]ustomer");
+        System.out.println("Enter the title or [] Prefix of your role:");
+        String input = in.next();
+        if(input.length() == 0){
+            input = "q";
+        }
+        char prefix = input.charAt(0);
+        Character.toUpperCase(prefix);
+        switch (prefix){
+            case 'M':
+                System.out.println("Manufacture employee");
+                ManufactureView mv = new ManufactureView();
+                mv.run();
+                break;
+        }
+    }
 
+    public static void main(String[] args) {
+        WelcomeView wv = new WelcomeView();
+        wv.run();
     }
 }
