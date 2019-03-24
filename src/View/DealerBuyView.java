51b4c080ba3aa635cea6799ae32d43ddc6d1945a
Eugene <exc8593@rit.edu>
2019-03-24 18:44:43 -0400

    descr. print out for dealerbuy and dealersell now

--- src/View/DealerBuyView.java
+++ src/View/DealerBuyView.java
@@ -2,29 +2,26 @@ package View;
 
 import java.util.Scanner;
 
-public class DealerView implements View{
-
+public class DealerBuyView implements View{
     @Override
     public void run() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello Dealer!\n\nWhat would you like to do?" +
-                "\n[s]ell a car\n[b]uy a car\n");
-        String input = in.next();
+        System.out.println("Buy Page\n-list all:\n\tmodels,brands,price" +
+                "\n-buy vin\n");
+        /*String input = in.next();
         char prefix = input.charAt(0);
         prefix = Character.toUpperCase(prefix);
         switch (prefix){
-            case 'S':
-                DealerSellView dsv = new DealerSellView();
-                dsv.run();
+            case 'L':
+
                 break;
             case 'B':
-                DealerBuyView dbv = new DealerBuyView();
-                dbv.run();
+
                 break;
             case 'Q':
                 return;
             default:
                 return;
-        }
+        }*/
     }
 }
