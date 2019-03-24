369e18926bfc83c778989adbfdb29f2fd5501eb0
david <dxb4791@rit.edu>
2019-03-24 19:08:37 -0400

    Update Make.java

--- src/Make.java
+++ src/Make.java
@@ -4,15 +4,17 @@
 public class Make {
     String makename;
     String model;
+    String D_ID;
 
     /**
      * regular make constructor
      * @param make make or brand of the car
      * @param model model of the car
      */
-    public Make(String make, String model){
+    public Make(String make, String model, String D_ID){
         this.makename = make;
         this.model = model;
+        this.D_ID=D_ID;
     }
 
     public Make(String[] data){
@@ -30,5 +32,9 @@ public class Make {
 
     public String getModel() {
         return model;
+    }
+
+    public String getD_ID() {
+        return D_ID;
     }
 }
