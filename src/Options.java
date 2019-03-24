8f382d80399857807dd0df6ba6fa691777c42d95
david <dxb4791@rit.edu>
2019-03-24 14:32:10 -0400

    Update Options.java

--- src/Options.java
+++ src/Options.java
@@ -1,2 +1,51 @@
 public class Options {
+    private final String o_id;
+    private final String transmission;
+    private final String color;
+    private final String engine;
+    private final String drive;
+    private final String interior;
+
+    public Options(String o_id,String transmission, String color,
+                   String engine,String drive,String interior){
+        this.o_id=o_id;
+        this.transmission=transmission;
+        this.color=color;
+        this.engine=engine;
+        this.drive=drive;
+        this.interior=interior;
+
+    }
+    public Options(String[] data){
+        this.o_id = data[0];
+        this.transmission = data[1];
+        this.color=data[2];
+        this.engine=data[3];
+        this.drive=data[4];
+        this.interior=data[5];
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDrive() {
+        return drive;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public String getO_id() {
+        return o_id;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
 }
