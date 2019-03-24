c606c3b1cd56f83d33e376503ffd446d53ab6073
david <dxb4791@rit.edu>
2019-03-24 18:54:14 -0400

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,5 @@
 /**
+ * class that handles the model attribute
  * @author dxb4791
  */
 
@@ -9,6 +10,16 @@ public class Model {
     private final String doors;
     private final String seats;
     private final String make_name;
+
+    /**
+     * regular Model constructor
+     * @param Name name of the model
+     * @param year year of the model
+     * @param c_class class of the car coupe, van ,..etc.
+     * @param seats number of the seats
+     * @param doors number of the doors
+     * @param makeName name of the make
+     */
     public Model(String Name,String year,String c_class,String seats,String doors,String makeName){
         this.Name=Name;
         this.year=year;
@@ -17,6 +28,11 @@ public class Model {
         this.doors=doors;
         this.make_name=makeName;
     }
+
+    /**
+     * csv file constructor
+     * @param data data from the csv
+     */
     public Model(String[] data){
         this.Name = data[0];
         this.year = data[1];
@@ -26,18 +42,34 @@ public class Model {
         this.make_name=data[5];
     }
 
+    /**
+     * make name getter
+     * @return
+     */
     public String getMakename() {
         return make_name;
     }
 
+    /**
+     * name getter
+     * @return
+     */
     public String getName() {
         return Name;
     }
 
+    /**
+     * class getter
+     * @return
+     */
     public String getC_class() {
         return c_class;
     }
 
+    /**
+     * doors getter
+     * @return
+     */
     public String getDoors() {
         return doors;
     }
