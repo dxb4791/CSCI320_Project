65eabd45e8997f4ef4929f7dca437b956ca1ddb5
david <dxb4791@rit.edu>
2019-03-24 21:15:17 -0400

    Update DealersManufactureView.java

--- src/View/DealersManufactureView.java
+++ src/View/DealersManufactureView.java
@@ -1,10 +1,22 @@
 package View;
 
+import db.DealerTable;
+import db.H2DatabaseMain;
+
 import java.util.Scanner;
 
 public class DealersManufactureView implements View{
     @Override
     public void run() {
+        H2DatabaseMain demo = new H2DatabaseMain();
+
+        //Hard drive location of the database
+        String location = "./database/database";
+        String user = "ceo";
+        String password = "test";
+
+        //Create the database connections, basically makes the database
+        demo.createConnection(location, user, password);
         Scanner in = new Scanner(System.in);
         System.out.println("Commands:\n-[L]ist dealers\n\tSort by sales" +
                 "\n-[F]ind Dealer\n\t-Car by VIN\n\t-Car by CLASS");
@@ -14,6 +26,7 @@ public class DealersManufactureView implements View{
         switch (prefix){
             case 'L':
                 System.out.println("This will list all dealers");
+                DealerTable.printDealerTable(demo.getConnection());
                 break;
             case 'F':
                 System.out.println("This will find dealers for cars by VIN or CLASS");
