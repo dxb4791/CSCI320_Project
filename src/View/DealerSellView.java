4adb808b4d60a4c4777e257fd3c9cf81f17277a9
david <dxb4791@rit.edu>
2019-03-24 21:15:16 -0400

    Update DealerSellView.java

--- src/View/DealerSellView.java
+++ src/View/DealerSellView.java
@@ -1,10 +1,22 @@
 package View;
 
+import db.CustomerTable;
+import db.H2DatabaseMain;
+
 import java.util.Scanner;
 
 public class DealerSellView implements View {
     @Override
     public void run() {
+        H2DatabaseMain demo = new H2DatabaseMain();
+
+        //Hard drive location of the database
+        String location = "./database/database";
+        String user = "ceo";
+        String password = "test";
+
+        //Create the database connections, basically makes the database
+        demo.createConnection(location, user, password);
         Scanner in = new Scanner(System.in);
         System.out.println("Sell Page\n\t-[S]ell to Customer" +
                 "\n\t-[L]ist all customers by: income, name\n");
@@ -17,6 +29,7 @@ public class DealerSellView implements View {
                 break;
             case 'L':
                 System.out.println("This will list all customers");
+                CustomerTable.printCustomerTable(demo.getConnection());
                 break;
             case 'Q':
                 return;
