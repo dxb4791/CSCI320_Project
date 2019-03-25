6a3433325b375cd1ab6c0ea00115a433e97c9325
david <dxb4791@rit.edu>
2019-03-24 21:15:08 -0400

    Update DealerBuyView.java

--- src/View/DealerBuyView.java
+++ src/View/DealerBuyView.java
@@ -1,10 +1,22 @@
 package View;
 
+import db.H2DatabaseMain;
+import db.VehicleTable;
+
 import java.util.Scanner;
 
 public class DealerBuyView implements View{
     @Override
     public void run() {
+        H2DatabaseMain demo = new H2DatabaseMain();
+
+        //Hard drive location of the database
+        String location = "./database/database";
+        String user = "ceo";
+        String password = "test";
+
+        //Create the database connections, basically makes the database
+        demo.createConnection(location, user, password);
         Scanner in = new Scanner(System.in);
         System.out.println("Buy Page\n-[L]ist all:\n\tmodels,brands,price" +
                 "\n-[B]uy vin\n");
@@ -14,6 +26,7 @@ public class DealerBuyView implements View{
         switch (prefix){
             case 'L':
                 System.out.println("This will list all models,brands,price");
+                VehicleTable.printVehicleTable(demo.getConnection());
                 break;
             case 'B':
                 System.out.println("This will buy vin");
