e97b0fba15b9e090411945348250f49bf313f97d
Eugene <exc8593@rit.edu>
2019-04-14 16:56:27 -0400

    query added for customer sort by income in dealer

--- src/db/CustomerTable.java
+++ src/db/CustomerTable.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * creates the customer table and SQL statments
@@ -230,6 +231,31 @@ public class CustomerTable {
             e.printStackTrace();
         }
 
+    }
+
+    public static void printCustomersByIncome(Connection conn) {
+        try {
+            Scanner input = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter a dealer id to find it's associated customers:\n");
+            String in = input.next();
+            String query = "select * from customer\n where d_id = " + in + "\norder by income desc;\n";
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+
+            while(result.next()){
+                System.out.printf("db.Customer %s: %s: %s: %s: %s: %s: %s: %s \n",
+                        result.getString(1),
+                        result.getString(2),
+                        result.getString(3),
+                        result.getString(4),
+                        result.getString(5),
+                        result.getString(6),
+                        result.getString(7),
+                        result.getString(8));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
